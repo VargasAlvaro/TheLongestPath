035abb6aa5a7facd9117f92844e40fba6b42df09
VargasAlvaro <97858197+VargasAlvaro@users.noreply.github.com>
2022-01-16 19:28:44 -0500

    Commit initial

---
+++ FormLongestSkiRun.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WFALongestSkiRun
+{
+    partial class FormLongestSkiRun
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.buttonProcces = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonLoadInput = new System.Windows.Forms.Button();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.labelResults = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelImputMatriz = new System.Windows.Forms.Label();
+            this.openFileDialogInput = new System.Windows.Forms.OpenFileDialog();
+            this.panelPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.Controls.Add(this.buttonProcces);
+            this.panelPrincipal.Controls.Add(this.buttonCancel);
+            this.panelPrincipal.Controls.Add(this.buttonLoadInput);
+            this.panelPrincipal.Controls.Add(this.textBoxResults);
+            this.panelPrincipal.Controls.Add(this.labelResults);
+            this.panelPrincipal.Controls.Add(this.textBoxInput);
+            this.panelPrincipal.Controls.Add(this.labelImputMatriz);
+            this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(494, 256);
+            this.panelPrincipal.TabIndex = 0;
+            // 
+            // buttonProcces
+            // 
+            this.buttonProcces.Location = new System.Drawing.Point(89, 216);
+            this.buttonProcces.Name = "buttonProcces";
+            this.buttonProcces.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcces.TabIndex = 6;
+            this.buttonProcces.Text = "Procces";
+            this.buttonProcces.UseVisualStyleBackColor = true;
+            this.buttonProcces.Click += new System.EventHandler(this.ButtonProcces_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(338, 216);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // buttonLoadInput
+            // 
+            this.buttonLoadInput.Location = new System.Drawing.Point(30, 80);
+            this.buttonLoadInput.Name = "buttonLoadInput";
+            this.buttonLoadInput.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadInput.TabIndex = 4;
+            this.buttonLoadInput.Text = "Load";
+            this.buttonLoadInput.UseVisualStyleBackColor = true;
+            this.buttonLoadInput.Click += new System.EventHandler(this.ButtonLoadInput_Click);
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(30, 147);
+            this.textBoxResults.Multiline = true;
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(451, 63);
+            this.textBoxResults.TabIndex = 3;
+            // 
+            // labelResults
+            // 
+            this.labelResults.AutoSize = true;
+            this.labelResults.Location = new System.Drawing.Point(27, 131);
+            this.labelResults.Name = "labelResults";
+            this.labelResults.Size = new System.Drawing.Size(42, 13);
+            this.labelResults.TabIndex = 2;
+            this.labelResults.Text = "Results";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(30, 54);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(451, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // labelImputMatriz
+            // 
+            this.labelImputMatriz.AutoSize = true;
+            this.labelImputMatriz.Location = new System.Drawing.Point(27, 38);
+            this.labelImputMatriz.Name = "labelImputMatriz";
+            this.labelImputMatriz.Size = new System.Drawing.Size(64, 13);
+            this.labelImputMatriz.TabIndex = 0;
+            this.labelImputMatriz.Text = "Imput Matriz";
+            // 
+            // openFileDialogInput
+            // 
+            this.openFileDialogInput.FileName = "openFileDialogInput";
+            // 
+            // FormLongestSkiRun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 256);
+            this.Controls.Add(this.panelPrincipal);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(510, 295);
+            this.MinimumSize = new System.Drawing.Size(510, 295);
+            this.Name = "FormLongestSkiRun";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Longest Ski Run";
+            this.Load += new System.EventHandler(this.FormLongestSkiRun_Load);
+            this.panelPrincipal.ResumeLayout(false);
+            this.panelPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonLoadInput;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Label labelResults;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelImputMatriz;
+        private System.Windows.Forms.OpenFileDialog openFileDialogInput;
+        private System.Windows.Forms.Button buttonProcces;
+    }
+}
+
---
+++ FormLongestSkiRun.cs
@@ -0,0 +1,540 @@
+﻿// <copyright file="FormLongestSkiRun.cs" company="EasySolutions">
+// Descripción de Funcionalidad   : Form to find the longest path in a matrix
+// Nombre Programador             : Ing. Alvaro Vargas
+// Creation date                  : 16 Enero 2022
+// </copyright>
+
+namespace WFALongestSkiRun
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Class FormLongestSkiRun
+    /// </summary>
+    public partial class FormLongestSkiRun : Form
+    {
+        #region Variables
+
+        /// <summary>
+        /// Indicates the rows of the loaded matrix
+        /// </summary>
+        private int rows;
+
+        /// <summary>
+        /// Indicates the columns of the loaded matrix
+        /// </summary>
+        private int columns;
+
+        /// <summary>
+        /// The loaded matrix
+        /// </summary>
+        private int[][] matrix = null;
+
+        #endregion Variables
+
+        #region Contructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormLongestSkiRun"/> class
+        /// </summary>
+        public FormLongestSkiRun()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion Contructors
+
+        #region Events
+
+        /// <summary>
+        /// FormLongestSkiRun Load event
+        /// </summary>
+        /// <param name="sender">Object sender</param>
+        /// <param name="e">Event e</param>
+        private void FormLongestSkiRun_Load(object sender, EventArgs e)
+        {
+            this.rows = 0;
+            this.columns = 0;
+        }
+
+        /// <summary>
+        /// ButtonLoadInput Button click event
+        /// </summary>
+        /// <param name="sender">Object sender </param>
+        /// <param name="e">Event e</param>
+        private void ButtonLoadInput_Click(object sender, EventArgs e)
+        {
+            this.LoadInput();
+        }
+
+        /// <summary>
+        /// Button Button click event
+        /// </summary>
+        /// <param name="sender">Object sender</param>
+        /// <param name="e">Event e</param>
+        private void ButtonProcces_Click(object sender, EventArgs e)
+        {
+            List<string> result = this.FindResults(this.matrix);
+
+            this.textBoxResults.Text = string.Empty;
+
+            foreach (var text in result)
+            {
+                this.textBoxResults.Text = this.textBoxResults.Text + text + "\r\n";
+            }
+        }
+
+        /// <summary>
+        /// ButtonCancel Button click event
+        /// </summary>
+        /// <param name="sender">Object sender</param>
+        /// <param name="e">Event e</param>
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            this.Clean();
+        }
+
+        #endregion Events
+
+        #region Methods
+
+        /// <summary>
+        /// Method for load matrix
+        /// </summary>
+        private void LoadInput()
+        {
+            try
+            {
+                DialogResult result = openFileDialogInput.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    this.textBoxInput.Text = openFileDialogInput.FileName;
+                    if (this.textBoxInput.Text != string.Empty)
+                    {
+                        this.textBoxInput.ReadOnly = true;
+
+                        if (this.LoadMatrix() == "OK")
+                        {
+                            this.textBoxResults.Text = string.Empty;
+                            this.textBoxResults.Text = "The matrix was loaded successfully";
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Failed to load matrix, please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            this.Clean();
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error chargin input: {ex.ToString()}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Method to clear form fields
+        /// </summary>
+        private void Clean()
+        {
+            try
+            {
+                this.textBoxInput.Text = string.Empty;
+                this.textBoxResults.Text = string.Empty;
+
+                this.textBoxInput.ReadOnly = false;
+                this.rows = 0;
+                this.columns = 0;
+                this.matrix = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error cleaning: {ex.ToString()}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion Methods
+
+        #region Functions 
+
+        /// <summary>
+        /// Function to load the array.
+        /// </summary>
+        /// <returns>Returns a matrix of type string</returns>
+        private string LoadMatrix()
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                string patch = this.textBoxInput.Text;
+                string[] lines = File.ReadAllLines(patch);
+
+                string[] rowsAndColumns = lines[0].Split(' ');
+
+                if (rowsAndColumns.Length != 2)
+                {
+                    MessageBox.Show($"The first line does not define the matrix, try another input.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return "Error";
+                }
+                else
+                {
+                    this.rows = Convert.ToInt32(rowsAndColumns[0]);
+                    this.columns = Convert.ToInt32(rowsAndColumns[1]);
+                }
+
+                if (this.rows == 0 || this.columns == 0)
+                {
+                    MessageBox.Show($"Cannot define an array with value zero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return "Error";
+                }
+
+                int[,] matrixLocal = new int[this.rows, this.columns];
+
+                for (int i = 0; i < this.rows; i++)
+                {
+                    string[] line = lines[i + 1].Split();
+
+                    for (int j = 0; j < this.columns; j++)
+                    {
+                        matrixLocal[i, j] = Convert.ToInt32(line[j].ToString());
+                    }
+                }
+
+                this.matrix = new int[this.rows][];
+
+                for (int i = 0; i < this.rows; i++)
+                {
+                    int[] vector = new int[this.columns];
+                    for (int j = 0; j < this.columns; j++)
+                    {
+                        vector[j] = matrixLocal[i, j];
+                    }
+
+                    this.matrix[i] = vector;
+                }
+
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error chargin input: {ex.ToString()}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "Error";
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        /// <summary>
+        /// Function that returns length of the longest path beginning with any cell
+        /// </summary>
+        /// <param name="mat">Loaded matrix</param>
+        /// <returns>Returns a list of texts</returns>
+        private List<string> FindResults(int[][] mat)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                int result = 1;
+                int majorValue = 0;
+                List<string> listResult = new List<string>();
+
+                // Create a lookup table and fill. All entries in it as -1
+                int[][] dp = RectangularArrays.ReturnRectangularIntArray(this.rows, this.rows);
+                for (int i = 0; i < this.rows; i++)
+                {
+                    for (int j = 0; j < this.columns; j++)
+                    {
+                        dp[i][j] = -1;
+                    }
+                }
+
+                string[][] matPath = RectangularArrays.ReturnRectangularStringArray(this.rows, this.columns);
+
+                ////Calculate longest path from all cells
+                for (int i = 0; i < this.rows; i++)
+                {
+                    for (int j = 0; j < this.columns; j++)
+                    {
+                        if (dp[i][j] == -1 || mat[i][j] > majorValue)
+                        {
+                            matPath[i][j] = mat[i][j].ToString();
+                            if (mat[i][j] > majorValue)
+                            {
+                                majorValue = mat[i][j];
+                            }
+
+                            this.FindLongestFromACell(i, j, mat, dp, matPath);
+                        }
+
+                        result = Math.Max(result, dp[i][j]);
+                    }
+                }
+
+                ////Calculate path
+                string resultPath = string.Empty;
+
+                for (int i = 0; i < this.rows; i++)
+                {
+                    for (int j = 0; j < this.columns; j++)
+                    {
+                        string actualPath = matPath[i][j];
+
+                        if (actualPath.Split('-').Length == result)
+                        {
+                            if (resultPath != string.Empty)
+                            {
+                                if (Convert.ToInt32(actualPath.Split('-')[0]) > Convert.ToInt32(resultPath.Split('-')[0]))
+                                {
+                                    resultPath = actualPath;
+                                }
+                            }
+                            else
+                            {
+                                resultPath = actualPath;
+                            }
+                        }
+                    }
+                }
+
+                ////Length of calculated path
+                string dropPath = string.Empty;
+                string[] dropPathA = resultPath.Split('-');
+                int dif = Convert.ToInt32(dropPathA[0]) - Convert.ToInt32(dropPathA[dropPathA.Length - 1]);
+
+                ////Add results
+                listResult.Add($"Length of calculated path: {result.ToString()}");
+                listResult.Add($"Drop of calculated path: {dif.ToString()}");
+                listResult.Add($"Calculated path: {resultPath}");
+
+                this.Cursor = Cursors.Default;
+
+                return listResult;
+            }
+            catch (Exception ex)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show($"Error finding results: {ex.ToString()}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new List<string>();
+            }
+        }
+
+        /// <summary>
+        /// Function that returns length of the longest path beginning with mat[i][j]
+        /// </summary>
+        /// <param name="i">Starting row to iterate</param>
+        /// <param name="j">starting column to iterate</param>
+        /// <param name="mat">Matrix with data</param>
+        /// <param name="dp">Matrix with long paths</param>
+        /// <param name="matPath">Matrix with calculated paths</param>
+        /// <returns>Returns length</returns>
+        private int FindLongestFromACell(int i, int j, int[][] mat, int[][] dp, string[][] matPath)
+        {
+            // Base case
+            if (i < 0 || i >= this.rows || j < 0 || j >= this.columns)
+            {
+                return 0;
+            }
+
+            //// If this subproblem is
+            //// already solved
+            ////if (dp[i][j] != -1)
+            ////{
+            ////    return dp[i][j];
+            ////}
+
+            ////To store the path lengths in all the four directions
+            int x = int.MinValue, y = int.MinValue, z = int.MinValue, w = int.MinValue;
+
+            ////Move in possible directions
+            if (j < this.rows - 1 && (mat[i][j] > mat[i][j + 1]))
+            {
+                if (matPath[i][j + 1] != null)
+                {
+                    string matPathAct = matPath[i][j + 1];
+                    string matPathNew = matPath[i][j] + "-" + mat[i][j + 1].ToString();
+
+                    string[] canPathAct = matPathAct.Split('-');
+                    string[] canPathNew = matPathNew.Split('-');
+
+                    if (canPathNew.Length > canPathAct.Length)
+                    {
+                        matPath[i][j + 1] = matPathNew;
+                    }
+
+                    if (canPathNew.Length == canPathAct.Length)
+                    {
+                        if (Convert.ToInt32(canPathNew[0]) > Convert.ToInt32(canPathAct[0]))
+                        {
+                            matPath[i][j + 1] = matPathNew;
+                        }
+                    }
+                }
+                else
+                {
+                    matPath[i][j + 1] = matPath[i][j] + "-" + mat[i][j + 1].ToString();
+                }
+
+                x = dp[i][j] = 1 + this.FindLongestFromACell(i, j + 1, mat, dp, matPath);
+            }
+
+            if (j > 0 && (mat[i][j] > mat[i][j - 1]))
+            {
+                if (matPath[i][j - 1] != null)
+                {
+                    string matPathAct = matPath[i][j - 1];
+                    string matPathNew = matPath[i][j] + "-" + mat[i][j - 1].ToString();
+
+                    string[] canPathAct = matPathAct.Split('-');
+                    string[] canPathNew = matPathNew.Split('-');
+
+                    if (canPathNew.Length > canPathAct.Length)
+                    {
+                        matPath[i][j - 1] = matPathNew;
+                    }
+
+                    if (canPathNew.Length == canPathAct.Length)
+                    {
+                        if (Convert.ToInt32(canPathNew[0]) > Convert.ToInt32(canPathAct[0]))
+                        {
+                            matPath[i][j - 1] = matPathNew;
+                        }
+
+                        if (canPathNew.Length == canPathAct.Length)
+                        {
+                            if (Convert.ToInt32(canPathNew[0]) > Convert.ToInt32(canPathAct[0]))
+                            {
+                                matPath[i][j - 1] = matPathNew;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    matPath[i][j - 1] = matPath[i][j] + "-" + mat[i][j - 1].ToString();
+                }
+
+                y = dp[i][j] = 1 + this.FindLongestFromACell(i, j - 1, mat, dp, matPath);
+            }
+
+            if (i > 0 && (mat[i][j] > mat[i - 1][j]))
+            {
+                if (matPath[i - 1][j] != null)
+                {
+                    string matPathAct = matPath[i - 1][j];
+                    string matPathNew = matPath[i][j] + "-" + mat[i - 1][j].ToString();
+
+                    string[] canPathAct = matPathAct.Split('-');
+                    string[] canPathNew = matPathNew.Split('-');
+
+                    if (canPathNew.Length > canPathAct.Length)
+                    {
+                        matPath[i - 1][j] = matPathNew;
+                    }
+
+                    if (canPathNew.Length == canPathAct.Length)
+                    {
+                        if (Convert.ToInt32(canPathNew[0]) > Convert.ToInt32(canPathAct[0]))
+                        {
+                            matPath[i - 1][j] = matPathNew;
+                        }
+                    }
+                }
+                else
+                {
+                    matPath[i - 1][j] = matPath[i][j] + "-" + mat[i - 1][j].ToString();
+                }
+
+                z = dp[i][j] = 1 + this.FindLongestFromACell(i - 1, j, mat, dp, matPath);
+            }
+
+            if (i < this.rows - 1 && (mat[i][j] > mat[i + 1][j]))
+            {
+                if (matPath[i + 1][j] != null)
+                {
+                    string matPathAct = matPath[i + 1][j];
+                    string matPathNew = matPath[i][j] + "-" + mat[i + 1][j].ToString();
+
+                    string[] canPathAct = matPathAct.Split('-');
+                    string[] canPathNew = matPathNew.Split('-');
+
+                    if (canPathNew.Length > canPathAct.Length)
+                    {
+                        matPath[i + 1][j] = matPathNew;
+                    }
+
+                    if (canPathNew.Length == canPathAct.Length)
+                    {
+                        if (Convert.ToInt32(canPathNew[0]) > Convert.ToInt32(canPathAct[0]))
+                        {
+                            matPath[i + 1][j] = matPathNew;
+                        }
+                    }
+                }
+                else
+                {
+                    matPath[i + 1][j] = matPath[i][j] + "-" + mat[i + 1][j].ToString();
+                }
+
+                w = dp[i][j] = 1 + this.FindLongestFromACell(i + 1, j, mat, dp, matPath);
+            }
+
+            // If none of the adjacent fours is one greater we will take 1
+            // otherwise we will pick maximum from all the four directions
+            dp[i][j] = Math.Max(x, Math.Max(y, Math.Max(z, Math.Max(w, 1))));
+
+            return dp[i][j];
+        }
+
+        #endregion Functions 
+
+        /// <summary>
+        /// Class to create rectangular arrays
+        /// </summary>
+        public static class RectangularArrays
+        {
+            /// <summary>
+            /// Function to return array of type integer
+            /// </summary>
+            /// <param name="size1">size for rows</param>
+            /// <param name="size2">size for columns</param>
+            /// <returns>Return array of type integer</returns>
+            public static int[][] ReturnRectangularIntArray(int size1, int size2)
+            {
+                int[][] newArray = new int[size1][];
+                for (int array1 = 0;
+                     array1 < size1; array1++)
+                {
+                    newArray[array1] = new int[size2];
+                }
+
+                return newArray;
+            }
+
+            /// <summary>
+            /// Function to return array of type string
+            /// </summary>
+            /// <param name="size1">size for rows</param>
+            /// <param name="size2">size for columns</param>
+            /// <returns>Return array of type string</returns>
+            public static string[][] ReturnRectangularStringArray(int size1, int size2)
+            {
+                string[][] newArray = new string[size1][];
+                for (int array1 = 0;
+                     array1 < size1; array1++)
+                {
+                    newArray[array1] = new string[size2];
+                }
+
+                return newArray;
+            }
+        }
+    }
+}
